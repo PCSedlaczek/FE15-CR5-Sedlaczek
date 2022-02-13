b47046ebf1f35980b346cad199f198aef64218be
Petra Christina <p.sedlaczek@drei.at>
2022-02-13 13:21:44 +0100

    Update Class methods

--- scripts/classes.js
+++ scripts/classes.js
@@ -3,6 +3,8 @@
 let animals = [];
 // let cats: Array<Cat> = [];
 // let dogs: Array<Dog> = [];
+// let birds: Array<Bird> = [];
+// let reptiles: Array<Reptile> = [];
 class Animal {
     constructor(name, gender, size, age, vaxx, img) {
         this.name = name;
@@ -13,8 +15,34 @@ class Animal {
         this.img = img;
         animals.push(this);
     }
+    cardStart() {
+        return `
+      <div class="col p-3">
+        <div class="card">
+          <img class="card-img-top" src="img/animals/${this.img}" alt="${this.name}">
+          <div class="card-header text-center bg-dark text-white">${this.name}</div>
+          <div class="card-body">
+            <ul class="list-unstyled mb-2">
+              <li>Gender: ${this.gender}</li>
+              <li>Age: ${this.age}</li>
+              <li>Size: ${this.size}</li>
+            </ul>
+            <!-- Vaccination status -->
+            <p class="card-text text-center">
+              <button type="button" class="btn">
+                Vaccination 
+                <i class="bi" role="img"></i>
+              </button>
+            </p>`;
+    }
+    cardEnd() {
+        return `
+          </div>
+        </div>
+      </div>`;
+    }
     display() {
-        return ``;
+        return this.cardStart() + this.cardEnd();
     }
 }
 class Cat extends Animal {
@@ -25,26 +53,37 @@ class Cat extends Animal {
         this.breedInfo = breedInfo;
         // cats.push(this);
     }
+    cardEnd() {
+        return `
+            <ul class="list-unstyled mb-2">
+              <li>Breed: ${this.breed}</li>
+              <li>Fur color: ${this.furColor}</li>
+              <li>
+                <a class="nav-link px-0" href="${this.breedInfo}">Breed info</a>
+              </li>
+            </ul>
+          ${super.cardEnd}`;
+    }
     display() {
-        return `<!-- Card -->
-    <div class="card" style="width: 18rem;">
-      <img class="card-img-top" src="img/animals/${this.img}" alt="${this.name}">
-      <div class="card-header text-center bg-dark text-white">${this.name}</div>
-      <div class="card-body">
-        <p class="card-text">Description</p>
-        <button  type="button" class="btn btn-success">Vaccinated: </button>
-      </div>
-    </div>`;
+        return super.cardStart() + this.cardEnd();
     }
 }
 class Dog extends Animal {
-    constructor(name, gender, size, age, vaxx, img, breed, trainingSkills) {
+    constructor(name, gender, size, age, vaxx, img, breed, training) {
         super(name, gender, size, age, vaxx, img);
         this.breed = breed;
-        this.trainingSkills = trainingSkills;
+        this.training = training;
         // dogs.push(this);
     }
+    cardEnd() {
+        return `
+            <ul class="list-unstyled mb-2">
+              <li>Breed: ${this.breed}</li>
+              <li>Training: ${this.training}</li>
+            </ul>
+          ${super.cardEnd}`;
+    }
     display() {
-        return ``;
+        return super.cardStart() + this.cardEnd();
     }
 }
--- scripts/classes.ts
+++ scripts/classes.ts
@@ -12,69 +12,109 @@ interface Animals {
 let animals: Array<Animal> = [];
 // let cats: Array<Cat> = [];
 // let dogs: Array<Dog> = [];
+// let birds: Array<Bird> = [];
+// let reptiles: Array<Reptile> = [];
 
 class Animal implements Animals {
   constructor(
-    public name: string, 
-    public gender: string, 
-    public size: string, 
-    public age: number, 
+    public name: string,
+    public gender: string,
+    public size: string,
+    public age: number,
     public vaxx: boolean,
-    public img: string) 
-  {
+    public img: string) {
     animals.push(this);
   }
 
-  display() {
-    return ``
+  cardStart(): string {
+    return `
+      <div class="col p-3">
+        <div class="card">
+          <img class="card-img-top" src="img/animals/${this.img}" alt="${this.name}">
+          <div class="card-header text-center bg-dark text-white">${this.name}</div>
+          <div class="card-body">
+            <ul class="list-unstyled mb-2">
+              <li>Gender: ${this.gender}</li>
+              <li>Age: ${this.age}</li>
+              <li>Size: ${this.size}</li>
+            </ul>
+            <!-- Vaccination status -->
+            <p class="card-text text-center">
+              <button type="button" class="btn">
+                Vaccination 
+                <i class="bi" role="img"></i>
+              </button>
+            </p>`
+  }
+
+  cardEnd(): string {
+    return `
+          </div>
+        </div>
+      </div>`
+  }
+
+  display(): string {
+    return this.cardStart() + this.cardEnd()
   }
 }
 
 class Cat extends Animal {
   constructor(
-    name: string, 
-    gender: string, 
-    size: string, 
-    age: number, 
+    name: string,
+    gender: string,
+    size: string,
+    age: number,
     vaxx: boolean,
     img: string,
     public breed: string,
     public furColor: string,
-    public breedInfo: string) 
-  {
+    public breedInfo: string) {
     super(name, gender, size, age, vaxx, img);
     // cats.push(this);
   }
 
-  display() {
-    return `<!-- Card -->
-    <div class="card" style="width: 18rem;">
-      <img class="card-img-top" src="img/animals/${this.img}" alt="${this.name}">
-      <div class="card-header text-center bg-dark text-white">${this.name}</div>
-      <div class="card-body">
-        <p class="card-text">Description</p>
-        <button  type="button" class="btn btn-success">Vaccinated: </button>
-      </div>
-    </div>`
+  cardEnd(): string {
+    return `
+            <ul class="list-unstyled mb-2">
+              <li>Breed: ${this.breed}</li>
+              <li>Fur color: ${this.furColor}</li>
+              <li>
+                <a class="nav-link px-0" href="${this.breedInfo}">Breed info</a>
+              </li>
+            </ul>
+          ${super.cardEnd}`
+  }
+
+  display(): string {
+    return super.cardStart() + this.cardEnd()
   }
 }
 
 class Dog extends Animal {
   constructor(
-    name: string, 
-    gender: string, 
-    size: string, 
+    name: string,
+    gender: string,
+    size: string,
     age: number,
-    vaxx: boolean, 
+    vaxx: boolean,
     img: string,
     public breed: string,
-    public trainingSkills: boolean) 
-  {
+    public training: boolean) {
     super(name, gender, size, age, vaxx, img);
     // dogs.push(this);
   }
 
+  cardEnd(): string {
+    return `
+            <ul class="list-unstyled mb-2">
+              <li>Breed: ${this.breed}</li>
+              <li>Training: ${this.training}</li>
+            </ul>
+          ${super.cardEnd}`
+  }
+
   display() {
-    return ``
+    return super.cardStart() + this.cardEnd()
   }
 }
